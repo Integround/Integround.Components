495748ddfa742f212dc8dd0ec22797680f5f2808
Jouko Virta <jouko.virta@integround.com>
2016-10-25 23:49:02 +0300

    Added new methods to the MessageLogStore interface

--- Integround.Components.Core/Integround.Components.Core/Log/IMessageLogStore.cs
+++ Integround.Components.Core/Integround.Components.Core/Log/IMessageLogStore.cs
@@ -7,6 +7,10 @@ namespace Integround.Components.Log
         LoggingLevel LoggingLevel { get; }
 
         Task<string> AddAsync<T>(T obj);
+        Task<string> AddAsync<T>(T obj, string fileName);
         Task<string> AddDebugAsync<T>(T obj);
+        Task<string> AddDebugAsync<T>(T obj, string fileName);
+
+        Task<int> CleanUp(int maxMessageAgeInDays);
     }
 }
